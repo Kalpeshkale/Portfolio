d57df1d7f40bce3fdab1aa4680571b38e22c3a62
Kalpesh <bhuiyan-dev3@cg.gov.in>
2025-05-29 23:03:05 +0530

    night commit

---
+++ src/components/Skills/skills.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './skills.css';
+import UIDesign from '../../assets/ui-design.png';
+import WebDesign from '../../assets/website-design.png';
+import AppDesign from '../../assets/app-design.png'
+ const Skills = () => {
+  return (
+    <section id="skills">
+        <span className="skillTitle">What do I</span>
+        <span className = "skillDesc"></span>
+
+    </section>
+  )
+}
+
+export default Skills
+
