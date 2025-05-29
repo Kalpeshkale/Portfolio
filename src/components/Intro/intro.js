e83cb07b342dc9b060d791c0acdcc78ed38864e4
Kalpesh <kalpeshkale34@gmail.com>
2025-05-29 17:24:10 +0530

    Initial commit

---
+++ src/components/Intro/intro.js
@@ -0,0 +1,20 @@
+import React from "react";  
+import './intro.css';
+import bg from '../../assets/image.png'
+import {Link} from 'react-scroll';
+import btnImg from '../../assets/hireme.png';
+const Intro = () => {
+    return (
+       <section id="intro">
+          <div className="introContent">
+             <span className="hello">Hello,</span>
+             <span className="introText">I'm <span className="introName">Kalpesh</span><br/>Website Designer</span>
+             <p className="introPara">I am a skilled web designer with experience in creating visually <br/>apealing and user  friendly websites.  </p>
+             <Link><button className="btn"><img src={btnImg} alt="Hire me" className='btnImg'/>Hire me</button></Link>
+          </div>
+          <img src={bg} alt="Profile" className="bg"/>
+       </section>
+    )
+}
+
+export default Intro
